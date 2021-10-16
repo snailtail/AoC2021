ae60062dd07c9139034b8bea15e7067ded756be2
Magnus Olofsson <magnusolofsson@outlook.com>
2021-10-16 11:44:53 +0200

    Add WarmUps

---
+++ WarmUps/AoC2017/AoC2017-01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace AoC2017
+{
+    public static class AoC201701
+    {
+        public static int Step1(string input)
+        {
+            int length = input.Length;
+            int comparer = 0;
+            int sum = 0;
+            for (int n = 0; n < length;n++)
+            {
+                if(n+1==length)
+                {
+                    comparer = 0;
+                }
+                else
+                {
+                    comparer = n + 1;
+                }
+
+                if(input.Substring(n,1)==input.Substring(comparer,1))
+                {
+                    sum +=int.Parse(input.Substring(n,1));
+                }
+            }
+            return sum;
+        }
+
+        public static int Step2(string input)
+        {
+            int length = input.Length;
+            int comparer = 0;
+            int step = length / 2;
+            int sum = 0;
+            for (int n = 0; n < length;n++)
+            {
+                if(n+step>=length)
+                {
+                    comparer = n+step-length;
+                }
+                else
+                {
+                    comparer = n + step;
+                }
+                if(input.Substring(n,1)==input.Substring(comparer,1))
+                {
+                    sum +=int.Parse(input.Substring(n,1));
+                }
+            }
+            return sum;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = File.ReadAllText("input.txt");
+            int result1 = AoC201701.Step1(input1);
+            Console.WriteLine($"Step 1 Result: {result1}");
+            int result2 = AoC201701.Step2(input1);
+            Console.WriteLine($"Step 2 Result: {result2}");
+        }
+    }
+}
---
+++ WarmUps/AoC2017/AoC2017-02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2017_02
+{
+    public static class AoC201702
+    {
+        public static Tuple<int, int> Process(string[] spreadSheet)
+        {
+            int checksum1 = 0;
+            int checksum2 = 0;
+            
+            foreach (string line in spreadSheet.Where(l => (!string.IsNullOrWhiteSpace(l) && !string.IsNullOrEmpty(l))).ToArray())
+            {
+                int lineMinVal = int.MaxValue;
+                int lineMaxVal = int.MinValue;
+                int[] intArray = Array.ConvertAll(line.Split(), s => int.Parse(s));
+                int numberCount = intArray.Length;
+                
+                #region step1 / outer loop
+                for (int n = 0; n < numberCount; n++)
+                {
+                    lineMaxVal = intArray[n] > lineMaxVal ? intArray[n] : lineMaxVal;
+                    lineMinVal = intArray[n] < lineMinVal ? intArray[n] : lineMinVal;
+                    
+                    #region step2 / inner loop
+                    if (n < numberCount - 1)
+                    {
+                        for (int k = n + 1; k < numberCount; k++)
+                        {
+                            double divresult = 0.0;
+                            int modresult = 0;
+                            if (intArray[n] > intArray[k])
+                            {
+                                divresult = (double)intArray[n] / (double)intArray[k];
+                                modresult = intArray[n] % intArray[k];
+                            }
+                            else
+                            {
+                                divresult = (double)intArray[k] / (double)intArray[n];
+                                modresult = intArray[k] % intArray[n];
+                            }
+                            if (modresult == 0)
+                            {
+                                checksum2 += (int)divresult;
+                            }
+                        }
+                    }
+                    #endregion
+                }
+                #endregion
+                checksum1 += (lineMaxVal - lineMinVal);
+            }
+            return Tuple.Create(checksum1, checksum2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] spreadSheet = File.ReadAllLines("input.txt").Where(l => string.IsNullOrEmpty(l) == false).ToArray<string>();
+            var result = AoC201702.Process(spreadSheet);
+            Console.WriteLine(result.Item1);
+            Console.WriteLine(result.Item2);
+        }
+    }
+}
+
