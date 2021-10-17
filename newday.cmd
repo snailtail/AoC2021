673ca1ef2c0a8945f899442799d3d1771e9bfd72
Magnus Olofsson <magnusolofsson@outlook.com>
2021-10-17 17:53:49 +0200

    Add Manhattan Helper

--- newday.cmd
+++ newday.cmd
@@ -2,7 +2,7 @@ git checkout main
 git pull -r
 git checkout -b day/%1
 dotnet new console -n %1
-cd %1 
+cd %1
 dotnet add reference ../Helpers/Helpers.csproj
 cd ..\Tests
 dotnet new xunit -n %1tests
@@ -13,4 +13,4 @@ dotnet add package Shouldly
 dotnet restore
 cd ../..
 git add .
-git commit -m "Add %1"
+git commit -m "Add new day %1"
