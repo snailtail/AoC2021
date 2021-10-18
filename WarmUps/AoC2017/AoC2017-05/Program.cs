78089ffc8af583f51b133f0685e5033aef9c31ee
Magnus Olofsson <magnusolofsson@outlook.com>
2021-10-18 09:10:08 +0200

    Add Aoc2017 Day 5 as Warmup

---
+++ WarmUps/AoC2017/AoC2017-05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace AoC2017_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AoC201705 myProcessor = new AoC201705();
+            string[] jumps = File.ReadAllLines("input.txt");
+            int step1 = myProcessor.ProcessList(jumps, false);
+            Console.WriteLine($"Step 1: {step1}");
+            int step2 = myProcessor.ProcessList(jumps, true);
+            Console.WriteLine($"Step 2: {step2}");
+        }
+    }
+}
