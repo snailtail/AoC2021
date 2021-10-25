4216ef8a2adadf91ac373ddd614436b29ae6a328
Magnus Olofsson <magnusolofsson@outlook.com>
2021-10-25 19:36:09 +0200

    Complete restructure after some borrowed inspiration

--- WarmUps/AoC2017/AoC2017-07/Program.cs
+++ WarmUps/AoC2017/AoC2017-07/Program.cs
@@ -7,13 +7,13 @@ namespace AoC2017_07
     {
         static void Main(string[] args)
         {
-            string[] programList = File.ReadAllLines("input.txt");
-            AoC201707 myProcessor = new AoC201707();
-            string step1 = myProcessor.getBottomProgram(programList);
+            string programString = File.ReadAllText("input.txt");
+            string step1 = AoC201707.Step1(programString);
             Console.WriteLine($"Step 1: {step1}");
+            string step2 = AoC201707.Step2(programString);
+            Console.WriteLine($"Step 2: {step2}");
 
-            // Step 2 - Probably move towards custom type for storing the programs, and LINQ to fetch relations.
-            // Needs to be some sort of a recursive loop until no more children are found - async? or parallell processing perhaps?
+
         }
     }
 }
