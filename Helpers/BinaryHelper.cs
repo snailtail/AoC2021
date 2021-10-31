0d1c756e21e7e3924fc3a5e1a2afa8ad8c80beb2
Magnus Olofsson <magnusolofsson@outlook.com>
2021-10-31 18:15:45 +0100

    Make BinaryHelper use length dynamically

--- Helpers/BinaryHelper.cs
+++ Helpers/BinaryHelper.cs
@@ -1,16 +1,18 @@
 using System;
-
-public class BinaryHelper
+namespace AdventOfCode.Helpers
 {
-     public long ConvertBinaryStringToLong(char[] binaryCharArray)
+    public class BinaryHelper
+    {
+        public long ConvertBinaryStringToLong(char[] binaryCharArray)
         {
             long returnValue = 0;
-            for(long n=0; n<binaryCharArray.Length; n++)
+            int theLength = binaryCharArray.Length -1;
+            for (long n = 0; n <=theLength ; n++)
             {
                 var thisBit = int.Parse(binaryCharArray[n].ToString());
-                if(thisBit==1)
+                if (thisBit == 1)
                 {
-                    var bitValue=(long)Math.Pow((2),(35 - n));
+                    var bitValue = (long)Math.Pow((2), (theLength - n));
                     returnValue += bitValue;
                 }
             }
@@ -28,16 +30,17 @@ public class BinaryHelper
         {
             const int mask = 1;
             var binary = string.Empty;
-            while(number > 0)
+            while (number > 0)
             {
                 // Logical AND the number and prepend it to the result string
                 binary = (number & mask) + binary;
                 number = number >> 1;
             }
-            while(binary.Length<36)
+            while (binary.Length < length)
             {
                 binary = "0" + binary;
             }
             return binary;
         }
+    }
 }
