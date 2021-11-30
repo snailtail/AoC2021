5f34b36f0fa2deee28c27fc14387dd103903f5e2
Magnus Olofsson <magnusolofsson@outlook.com>
2021-11-30 19:46:56 +0100

    Add new day %1

---
+++ %1/%1tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace _1tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ newday.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git checkout main
+git pull -r
+git checkout -b day/%1
+dotnet new console -n %1
+cd %1
+dotnet add reference ../Helpers/Helpers.csproj
+cd ..\Tests
+dotnet new xunit -n %1tests
+cd %1tests
+dotnet add reference ../../%1/%1.csproj
+dotnet add reference ../../Helpers/Helpers.csproj
+dotnet add package Shouldly
+dotnet restore
+cd ../..
+git add .
+git commit -m "Add new day %1"
