5a084857a8f1b76f6bd2fdf89cef026865af976a
Magnus Olofsson <magnusolofsson@outlook.com>
2021-12-01 06:33:06 +0100

    Solved Day 1 (not cleaned up and no tests)

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode.Helpers;
+using System.Linq;
+
+namespace AdventOfCode.Y2021
+{
+    public static class Day01
+    {
+        public static int Step1(string[] lines)
+        {
+            int[] measurements = lines.Select(l => int.Parse(l)).ToArray();
+            int counter = 0;
+            for (int n = 0; n < measurements.Length; n++)
+            {
+                if (n + 1 < measurements.Length)
+                {
+                    if (measurements[n + 1] > measurements[n])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        public static int Step2(string[] lines)
+        {
+            int[] measurements = lines.Select(l => int.Parse(l)).ToArray();
+            int counter = 0;
+            int lastsum = 0;
+            for (int n = 0; n < measurements.Length - 2; n++)
+            {
+                if (n > 0)
+                {
+                    if ((measurements[n] + measurements[n + 1] + measurements[n + 2]) > lastsum)
+                    {
+                        counter++;
+                    }
+                }
+                lastsum = (measurements[n] + measurements[n + 1] + measurements[n + 2]);
+            }
+            return counter;
+        }
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            Console.WriteLine(Step1(lines));
+            Console.WriteLine(Step2(lines));
+        }
+    }
+}
+
--- tests/day01tests/UnitTest1.cs
+++ tests/day01tests/UnitTest1.cs
@@ -1,12 +1,18 @@
 using Xunit;
+using Shouldly;
+using AdventOfCode.Y2021;
+using AdventOfCode.Helpers;
 
 namespace day01tests;
 
-public class UnitTest1
+
+public class Day01Tests
 {
     [Fact]
     public void Test1()
     {
+
+        Day01.Step1().ShouldBe(-1);
 
     }
 }
