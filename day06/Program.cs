bc58b49bfeeff995f50f50f48c552a209b96950a
Magnus Olofsson <magnus.p.olofsson@mio.se>
2021-12-06 10:56:24 +0100

    Cleaned up.

--- day06/Program.cs
+++ day06/Program.cs
@@ -5,58 +5,50 @@
         private static Dictionary<int, long> fishAgeGroups = new();
         public static void Main(string[] args)
         {
-            //step 1
+            //Step 1, age the fishies 80 days
             resetFishAgeGroups();
-            for (int i = 0; i < 80; i++)
-            {
-                AgeFish();
-            }
-            long sumStep1 = 0;
-            foreach (KeyValuePair<int, long> kv in fishAgeGroups)
-            {
-                sumStep1 += kv.Value;
-            }
+            long sumStep1 = AgeFish(80);
             Console.WriteLine(sumStep1);
 
-            //Step 2, reset and do over
-            resetFishAgeGroups();
-            for (int i = 0; i < 256; i++)
+            //Step 2, age the fishies up to 256 days (we already did 80, so let's just continue on with the remaining 176)
+            long sumStep2 = AgeFish(176);
+            Console.WriteLine(sumStep2);
+        }
+
+
+        public static long AgeFish(int repetitions)
+        {
+            long sum = 0;
+            for (int i = 0; i < repetitions; i++)
             {
-                AgeFish();
+                long new8 = fishAgeGroups[0];
+                long add6 = fishAgeGroups[0];
+                long new0 = fishAgeGroups[1];
+                long new1 = fishAgeGroups[2];
+                long new2 = fishAgeGroups[3];
+                long new3 = fishAgeGroups[4];
+                long new4 = fishAgeGroups[5];
+                long new5 = fishAgeGroups[6];
+                long new6 = fishAgeGroups[7] + add6;
+                long new7 = fishAgeGroups[8];
+
+                fishAgeGroups[0] = new0;
+                fishAgeGroups[1] = new1;
+                fishAgeGroups[2] = new2;
+                fishAgeGroups[3] = new3;
+                fishAgeGroups[4] = new4;
+                fishAgeGroups[5] = new5;
+                fishAgeGroups[6] = new6;
+                fishAgeGroups[7] = new7;
+                fishAgeGroups[8] = new8;
             }
-            long sumStep2 = 0;
             foreach (KeyValuePair<int, long> kv in fishAgeGroups)
             {
-                sumStep2 += kv.Value;
+                sum += kv.Value;
             }
-            Console.WriteLine(sumStep2);
-
+            return sum;
         }
 
-        public static void AgeFish()
-        {
-            long new8 = fishAgeGroups[0];
-            long add6 = fishAgeGroups[0];
-            long new0 = fishAgeGroups[1];
-            long new1 = fishAgeGroups[2];
-            long new2 = fishAgeGroups[3];
-            long new3 = fishAgeGroups[4];
-            long new4 = fishAgeGroups[5];
-            long new5 = fishAgeGroups[6];
-            long new6 = fishAgeGroups[7] + add6;
-            long new7 = fishAgeGroups[8];
-
-            fishAgeGroups[0] = new0;
-            fishAgeGroups[1] = new1;
-            fishAgeGroups[2] = new2;
-            fishAgeGroups[3] = new3;
-            fishAgeGroups[4] = new4;
-            fishAgeGroups[5] = new5;
-            fishAgeGroups[6] = new6;
-            fishAgeGroups[7] = new7;
-            fishAgeGroups[8] = new8;
-
-        }
 
         private static void resetFishAgeGroups()
         {
@@ -69,11 +61,8 @@
             // add the initial fishes states
             foreach (var i in fishes)
             {
-
                 fishAgeGroups[i]++;
             }
         }
     }
-
-
 }
