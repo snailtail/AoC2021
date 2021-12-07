bee5fc0ed0b38e26d86f09668848abb8d3cdf284
Magnus Olofsson <magnusolofsson@outlook.com>
2021-12-07 07:05:23 +0100

    Day/day07 (#24)
    * Add new day day07
    
    * Solved Step 1 of Day 7
    
    * Solved step 2
    
    * Cleaned up

---
+++ day07/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace AdventOfCode
+{
+    public static class Day07
+    {
+
+        public static void Main(string[] args)
+        {
+            int[] crabPositions = File.ReadAllText("input.txt").Split(",").Select(p => int.Parse(p)).ToArray();
+            crabGroup myCrabs = new crabGroup(crabPositions);
+            //Console.WriteLine(myCrabs);
+            (int position1, long fuelcost1) = myCrabs.getBestPosition(false);
+            (int position2, long fuelcost2) = myCrabs.getBestPosition((true));
+            Console.WriteLine($"Best position: {position1}, fuelcost: {fuelcost1}");
+            Console.WriteLine($"Best position: {position2}, fuelcost: {fuelcost2}");
+        }
+    }
+
+    public class Crab
+    {
+        public int Position { get; set; }
+        public Crab(int position)
+        {
+            Position = position;
+        }
+    }
+    public class crabGroup
+    {
+        public int[] Crabs { get; set; }
+
+        public crabGroup(int[] crabPositions)
+        {
+            Crabs = crabPositions;
+        }
+        private int fuelConsumption(int distance, bool step2FuelConsumption)
+        {
+            int fuelConsumption = 0;
+            for (int y = 1; y <= distance; y++)
+            {
+                fuelConsumption += y;
+            }
+            if (step2FuelConsumption)
+            {
+
+                return fuelConsumption;
+
+            }
+            else
+            {
+                return distance;
+            }
+        }
+        public (int, long) getBestPosition(bool step2FuelConsumption)
+        {
+            int thisPosition = 0;
+            long fuelCost = long.MaxValue;
+            int bestPosition = 0;
+            int maxPosition = Crabs.Max();
+            int minPosition = Crabs.Min();
+
+            for (int x = minPosition; x <= maxPosition; x++)
+            {
+                long loopFuelCost = 0;
+                thisPosition = x;
+                foreach (var crab in Crabs)
+                {
+                    loopFuelCost += fuelConsumption(Math.Abs(x - crab), step2FuelConsumption);
+                }
+                if (loopFuelCost < fuelCost)
+                {
+                    bestPosition = thisPosition;
+                    fuelCost = loopFuelCost;
+                }
+            }
+            return (bestPosition, fuelCost);
+        }
+
+        public override string ToString()
+        {
+            string crabData = string.Empty;
+            foreach (var crab in Crabs)
+            {
+                crabData += $"{crab}, ";
+            }
+            return crabData;
+
+        }
+    }
+}
