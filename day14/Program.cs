8de573b2cabf87e69b9d98f0958be5a098280c1c
Magnus Olofsson <magnusolofsson@outlook.com>
2021-12-16 07:23:44 +0100

    Clean up

--- day14/Program.cs
+++ day14/Program.cs
@@ -9,14 +9,15 @@
         string[] lines = File.ReadAllLines("input.txt").Where(l => !string.IsNullOrEmpty(l)).ToArray();
 
         string inputPolymers = lines[0];
-
+        
+        // add rules
         foreach (var line in lines.Skip(1))
         {
             string[] ruleParts = line.Split(" -> ");
             Rules.Add(ruleParts[0], ruleParts[1]);
         }
 
-
+        // add initial pairs
         for (int n = 0; n < inputPolymers.Length - 1; n++)
         {
             var pair = inputPolymers.Substring(n, 2);
@@ -24,16 +25,22 @@
             Pairs[pair]++;
         }
 
+        // add polymers with count
         for (int n = 0; n < inputPolymers.Length; n++)
         {
             Polymercount.TryAdd(inputPolymers[n].ToString(), 0);
             Polymercount[inputPolymers[n].ToString()]++;
         }
 
-        for (int n = 0; n < 40; n++)
+        //loop x amount of times
+        for (int x = 0; x < 40; x++)
         {
+            //get the keys, since we can't loop over the dictionary itself and simultaneously modify it
             List<string> pairKeys = new List<string>(Pairs.Keys);
+            
+            // make a new Dict for the updated polymerPairs
             var updatedPolymerPairs = new Dictionary<string, long>();
+
             
             foreach (var pair in pairKeys)
             {
